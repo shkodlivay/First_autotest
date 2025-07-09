3bd7a2a9b3a1fb62edc6a998c320ea63ca227897
shkodlivay <garira21@mail.ru>
2025-07-10 02:46:45 +0300

    Delete exception handling for test

---
+++ FormTest.java
@@ -0,0 +1,34 @@
+package main;
+
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Test;
+
+import pages.FormPage;
+
+public class FormTest extends BaseSuite {
+
+    @Test
+    @Description("Тест заполнения формы регистрации")
+    public void fillRegistrationForm() {
+        String username = TestCredentials.getCredential("testLoginUsername");
+        String password = TestCredentials.getLoginPassword();
+        String email = "testuser@testmail.ru";
+        String date = "12-12-2012";
+        String languageLevel = "Продвинутый";
+
+        FormPage formPage = new FormPage(driver);
+        formPage.open();
+        formPage.inputUsername(username);
+        formPage.inputEmail(email);
+        formPage.inputPassword(password);
+        formPage.confirmPassword(password);
+        formPage.validatePasswords();
+        formPage.inputDate(date);
+
+        formPage.selectLanguageLevel(languageLevel);
+
+        formPage.sendForm();
+
+        formPage.validateOutputData(username, email, date, languageLevel);
+    }
+}
